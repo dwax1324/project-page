1f09f55ff7d268b41c71917ef271b1e36b5fcecd
dwax1324 <dwax1324@gmail.com>
2021-07-08 06:03:58 +0900

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,20 +23,38 @@ function useCard() {
 
 
 const imgURL = "https://raw.githubusercontent.com/dwax1324/10print/master/%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C%20(1).png"
+
 const Card = () => {
+    const getId = (e) => {
+        console.log(e.target);
+    }
 
     let cards = useCard()
     return (
         <div className="card-container">
+
             {
                 cards?.map((c) =>
-                    <div className="card-box">
-                        <img src={c?.img} className="card-img">
-
-                        </img>
-                        <div className="card-title">
-                            {c?.title}
+                    <div key={c?.id}>
+                        <input type="checkbox" id="popup" />
+                        <label for="popup">
+                            <div className="card-box" onClick={(e) => getId(e)}>
+                                <img src={c?.img} className="card-img" />
+                                <div className="card-title">
+                                    {c?.title}
+                                </div>
+                            </div>
+                        </label>
+                        <div>
+                            <div>
+                                <label for="popup" />
+                                {c?.title}
+                                {c?.description}
+                                <img src={c?.img} />
+                            </div>
+                            <label for="popup"></label>
                         </div>
+
                     </div>
                 )
 
